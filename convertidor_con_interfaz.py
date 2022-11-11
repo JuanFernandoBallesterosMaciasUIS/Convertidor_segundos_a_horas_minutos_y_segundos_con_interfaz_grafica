58c250f6eb9c3987ae94c7228cf2de6a13fcffd2
juanfernando <https://github.com/JuanFernandoBallesterosMaciasUIS>
2022-11-11 12:17:21 -0500

    programa terminado

--- convertidor_con_interfaz.py
+++ convertidor_con_interfaz.py
@@ -1,6 +1,5 @@
 from tkinter import *
-
-
+from tkinter import messagebox
 
 
 def convertir():
@@ -11,14 +10,25 @@ def convertir():
 
     m2 = int(m1)
     s1 = int((m1-m2)*60)
+    if h1 == 1:
+        t_resultados.insert(INSERT, "De los " + x.get() + " segundos, serian:\n"+ str(h1) + " hora con " + str(m2) + " minutos y " + str(s1) + " segundos.")
+    else:
+        t_resultados.insert(INSERT, "De los " + x.get() + " segundos, serian:\n"+ str(h1) + " horas con " + str(m2) + " minutos y " + str(s1) + " segundos.")
+        
+def borrar():
+    x.set("")
+    t_resultados.delete("1.0","end")
+    
+def salir():
+    messagebox.showinfo("Conversión 1.0", "La app se cerrará...")
+    ventana_principal.destroy()
 
-    t_resultados.insert(INSERT, "De los " + x.get() + " segundos, serian: "+ str(h1) + " hora con " + str(m2) + " minutos y " + str(s1) + " segundos")
     
 
 ventana_principal = Tk()
 ventana_principal.title("Convertidor")
 ventana_principal.geometry("400x450")
-#ventana_principal.resizable(0,0)
+ventana_principal.resizable(0,0)
 ventana_principal.config(bg="snow")
 
 
@@ -56,17 +66,17 @@ bt_convertir = Button(ventana_principal, text="Convertir",bg="slategray4", fg="l
 bt_convertir.place(x=150, y=205, width=100, height=30)
 
 #Boton borrar
-bt_borrar = Button(ventana_principal, text="Borrar",bg="slategray4", fg="light grey")
+bt_borrar = Button(ventana_principal, text="Borrar",bg="slategray4", fg="light grey",command= borrar)
 bt_borrar.place(x=25, y=205, width=100, height=30)
 
 # Boton salir
-bt_salir = Button(ventana_principal, text="Salir",bg="slategray4", fg="light grey")
+bt_salir = Button(ventana_principal, text="Salir",bg="slategray4", fg="light grey", command= salir)
 bt_salir.place(x=275, y=205, width=100, height=30)
 
 
 #area de texto para resultados
 t_resultados = Text(ventana_principal)
-t_resultados.config(bg = "slategray4", width = 22 , height = 5, fg="white", font=("Rubik",20))
+t_resultados.config(bg = "slategray4", width = 25 , height = 6, fg="white", font=("Rubik",17))
 t_resultados.place(x = 10, y = 260)
 
 ventana_principal.mainloop()
